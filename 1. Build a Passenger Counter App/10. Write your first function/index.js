0ff176a5b32ec33b135315f32eac0fe317653ba5
Anthony Dupont <anthony.dupont89@gmail.com>
2025-09-26 20:25:35 +0200

    1.10

--- 1. Build a Passenger Counter App/10. Write your first function/index.js
+++ 1. Build a Passenger Counter App/10. Write your first function/index.js
@@ -1,3 +1,13 @@
 // Create a function (you decide the name) that logs out the number 42 to the console
 // Call/invoke the function
 
+// Arrow function
+// let ultimateAnswer = () => {
+//   console.log(42);
+// }
+
+function ultimateAnswer() {
+  console.log(42);
+}
+
+ultimateAnswer()
