bc6ab786d43ee033b8f18454f7d98b3af37be547
Anthony Dupont <anthony.dupont89@gmail.com>
2025-09-26 20:45:17 +0200

    1.11

--- 1. Build a Passenger Counter App/11. Write a function that logs the sum/index.js
+++ 1. Build a Passenger Counter App/11. Write a function that logs the sum/index.js
@@ -4,3 +4,16 @@ let lap3 = 36
 
 // Create a function that logs out the sum of all the lap times
 
+// let sum = (a, b, c) => {
+//   console.log(a + b + c);
+// }
+
+// sum(lap1, lap2, lap3)
+
+
+function logLapTime () {
+  let totalTime = lap1 + lap2 + lap3
+  console.log(totalTime);
+}
+
+logLapTime()
