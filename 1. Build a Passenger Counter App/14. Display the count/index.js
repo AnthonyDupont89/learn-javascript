d780cb394532b3812773c4c1cc6a8f66f9769152
Anthony Dupont <anthony.dupont89@gmail.com>
2025-09-27 15:13:53 +0200

    1.14

--- 1. Build a Passenger Counter App/14. Display the count/index.js
+++ 1. Build a Passenger Counter App/14. Display the count/index.js
@@ -2,11 +2,14 @@
 
 // change the count-el in the HTML to reflect the new count
 
+// camelCase
+let countEl = document.getElementById("count-el") // pass in arguments
 let count = 0
 
 function increment() {
     count = count + 1
-    console.log(count)
+    countEl.innerText = count
+    // console.log(count)  
 }
 
 
