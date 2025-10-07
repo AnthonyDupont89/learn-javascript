c491b235642fbe70b804191d2d5533c8140749e9
Anthony Dupont <anthony.dupont89@gmail.com>
2025-10-07 22:45:44 +0200

    2.01

--- 2. Practice time - part 1/01. Variables practice/index.js
+++ 2. Practice time - part 1/01. Variables practice/index.js
@@ -1,7 +1,9 @@
 // Create two variables, firstName and lastName
-
+let firstName = "Anthony"
+let lastName = "Dupont"
 // Concatenate the two variables into a third variable called fullName
-
+let fullName = firstName + " " + lastName
 // Log fullName to the console
+console.log(fullname);
 
 
