4a7de2dd76adc29fbdfc937d7bd47e8c07c381ca
Anthony Dupont <anthony.dupont89@gmail.com>
2025-10-15 17:22:19 +0200

    3.02

--- 3. Build a Blackjack game/02. Add the firstCard, secondCard, and sum/index.js
+++ 3. Build a Blackjack game/02. Add the firstCard, secondCard, and sum/index.js
@@ -1,4 +1,21 @@
 // 1. Create two variables, firstCard and secondCard. 
 // Set their values to a random number between 2-11
 
+
 // 2. Create a variable, sum, and set it to the sum of the two cards
+
+let firstCard = Math.floor(Math.random() * 10) + 2
+let secondCard = Math.floor(Math.random() * 10) + 2
+
+let sum = firstCard + secondCard
+
+console.log(`${firstCard} + ${secondCard} = ${sum}`);
+
+
+// SOLUTION
+// let firstCard = 6
+// let secondCard = 9
+
+// let sum = firstCard + secondCard
+
+// console.log(sum);
