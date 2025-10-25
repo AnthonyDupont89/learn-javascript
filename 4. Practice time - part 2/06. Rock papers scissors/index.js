720a068558bc89cb34a3fd870bfdcd87036a87fc
Anthony Dupont <anthony.dupont89@gmail.com>
2025-10-25 16:41:59 +0200

    4.06

--- 4. Practice time - part 2/06. Rock papers scissors/index.js
+++ 4. Practice time - part 2/06. Rock papers scissors/index.js
@@ -2,4 +2,16 @@ let hands = ["rock", "paper", "scissor"]
 
 // Create a function that returns a random item from the array
 
+// function getHand() {
+//   let randomIndex = Math.floor(Math.random()*hands.length)
+//   return randomIndex
+// }
 
+// console.log(hands[getHand()]);
+
+function getHand() {
+  let randomIndex = Math.floor(Math.random()*3)
+  return hands[randomIndex]
+}
+
+console.log(getHand());
